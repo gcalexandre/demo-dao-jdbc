db1fb2e8c79df0cd794fcbff98fb128a480cd9b2
gcalexandre <gcalexandre.castilho@gmail.com>
2019-02-07 00:10:29 -0200

    DAO, FindById implementation, DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
